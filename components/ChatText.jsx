86def6ba99a7965a527a97d2cbd5db9af8f28d4f
sajadmhy <sajad.mahyaei@gmail.com>
2022-06-21 18:48:29 +0430

    Create chat messages

--- components/ChatText.jsx
+++ components/ChatText.jsx
@@ -1,7 +1,15 @@
+import styles from '../styles/ChatText.module.css';
+import { Data } from './Data';
+
 export default function ChatText() {
     return (
-        <>
-        <div>Hi there</div>
-        </>
+        <div className={styles.container}>
+            {Data.map((value , index) => 
+                <div key={index} className={value.me ? styles.myMessage : styles.hisMessage}>
+                    <p>{value.text}</p>
+                    <p className={styles.date}>{value.date}</p>
+                </div>
+            )}
+        </div>
     )
 }
---
+++ components/Data.jsx
@@ -0,0 +1,32 @@
+export const Data = [
+    {
+        me: true,
+        text: "Hey Samuel, can you update the calendar invite with a google meet link?",
+        date: "03:43 PM 15 Jun, 2022",
+    },
+    {
+        me: false,
+        text: "Hello Sajad, ADPList has an internal video and the link will be in the invite. Is there any reason why you are asking for a Google Meet ?",
+        date: "03:48 PM 15 Jun, 2022",
+    },
+    {
+        me: true,
+        text: "Oh sorry didn't know about that",
+        date: "03:49 PM 15 Jun, 2022",
+    },
+    {
+        me: true,
+        text: "thought all the sessions are through google meet",
+        date: "03:50 PM 15 Jun, 2022",
+    },
+    {
+        me: true,
+        text: "thank you for letting me know",
+        date: "03:50 PM 15 Jun, 2022",
+    },
+    {
+        me: false,
+        text: "Mentors have the flexibility to pick an option whether Google Meet or ADPList internal video",
+        date: "03:50 PM 15 Jun, 2022",
+    }
+]
